2f1b1d33d685c43b8f6ddd346166a935460f792b
Augusto Nunes <augusto.cesar@gmail.com>
2020-09-15 00:18:50 -0300

    terça braba

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,31 @@ const Button = (props) => (
     </button>
 )
 
-const DisplayValue = (props) =>  <em>{props.value}</em>
+const Statistic = (props) => {
+    return (
+        <tr>
+            <td> {props.text} </td>
+            <td> {props.stats} </td>
+            <td> {props.alt_text} </td>
+        </tr>
+    )
+}
+
+const Statistics = ({good, neutral, bad} ) => {
+    return (
+        <table>
+          <tbody>
+            <Statistic text={"Good reviews:"} stats={good} />
+            <Statistic text={"Neutral reviews:"} stats={neutral} />
+            <Statistic text={"Bad reviews:"} stats={bad} />
+            <Statistic text={"Neutral reviews:"} stats={neutral} />
+            <Statistic text={"All reviews:"} stats={good+neutral+bad}/>
+            <Statistic text={"Average: "} stats={(good*1+neutral*0+(-1)*bad)/(good+neutral+bad)} />
+            <Statistic text={"Positive:"} stats={good/(good+neutral+bad)} alt_text={'%'} />
+        </tbody>
+        </table>
+)
+}
 
 const App = () => {
     // save clicks of each button to own state
@@ -17,18 +41,16 @@ const App = () => {
 
     const handleGoodClick = () => {
         setGood(good+1)
-        console.log('good')
     }
 
     const handleNeutralClick = () => {
         setNeutral(neutral + 1)
-        console.log('neutral')
     }
 
     const handleBadClick = () => {
         setBad(bad + 1)
-        console.log('bad')
     }
+    if (good+neutral+bad !== 0)
     return (
         <div>
             <h1>Give Feedback</h1>
@@ -36,12 +58,20 @@ const App = () => {
             <Button handleClick={() => handleNeutralClick()} text="Neutral" /> 
             <Button handleClick={() => handleBadClick()} text="Bad" /> 
         <h1>statistics</h1>
-            <div> Good reviews: <DisplayValue value={good} /></div>
-            <div> Neutral reviews: <DisplayValue value={neutral} /></div>
-            <div> Bad reviews: <DisplayValue value={bad} /></div>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
 
     )
+    else 
+        return (
+            <div>
+                <h1>Give Feedback</h1>
+                <Button handleClick={() => handleGoodClick()} text="Good" /> 
+                <Button handleClick={() => handleNeutralClick()} text="Neutral" /> 
+                <Button handleClick={() => handleBadClick()} text="Bad" /> 
+                <h1>No feedback given!</h1>
+            </div>
+        )
 }
 
 ReactDOM.render(<App />,
