7e7661f667fb036e8f4c103ac5333776d689ce92
Augusto Nunes <augusto.cesar@gmail.com>
2020-09-17 01:53:31 -0300

    madruga

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Course = ({course}) => {
+const Header = ({ course }) => {
+      return (
+              <h1>{course.name}</h1>
+            )
+}
+
+
+const Part = (props) => {
+      return (
+              <p>
+                {props.part.name} {props.part.exercises}
+              </p>    
+            )
+}
+
+const Content = ({ course }) => {
+      return (
+              <div>
+                <Part part={course.parts[0]} />
+                <Part part={course.parts[1]} />
+                <Part part={course.parts[2]} />
+              </div>
+            )
+}
+return (
+    <div>
+        <Header course={course} />
+        <Content course={course} />
+    </div>
+)
+}    
+    
 
 const App = () => {
       const course = {
           id: 1,
-          name: 'Half Stack application development',
-          parts: [
-            {
-                            name: 'Fundamentals of React',
-                            exercises: 10
-                          },
-                {
-                            name: 'Using props to pass data',
-                            exercises: 7
-                          },
-                {
-                            name: 'State of a component',
-                            exercises: 14
-                          }
-              ]
-          }
+              name: 'Half Stack application development',
+              parts: [
+                        {
+                                    name: 'Fundamentals of React',
+                                    exercises: 10
+                                  },
+                        {
+                                    name: 'Using props to pass data',
+                                    exercises: 7
+                                  },
+                        {
+                                    name: 'State of a component',
+                                    exercises: 14
+                                  }
+                      ]
+            }
 
       return (
               <div>
+                <Course course={course} />
               </div>
             )
 }
